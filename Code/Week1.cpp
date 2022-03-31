ae47af7f7976817855959e488977e35646871905
bumsu-kim <49388481+bumsu-kim@users.noreply.github.com>
2022-03-31 16:46:25 -0700

    Week1 Thursday updates
    Slides added, Week1.cpp updated

--- Code/Week1.cpp
+++ Code/Week1.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 
 int main() {
+	// Week 1 Tuesday
 	cout << "Hello, World!" << endl;
+
+	// Week 1 Thursday
+	int x = INT_MIN; // the minimum possible integer for type int
+	cout << x << endl << -x << endl;
+	// prints out x and -x
+
+	// when x > INT_MIN, no problems at all
+	// however, when x == INT_MIN, we have an overflow:
+	// (-INT_MIN) is greater than INT_MAX (maximum possible integer for int)
+	// and thus we have a negative number when you run the program.
+
 	return 0;
 }
