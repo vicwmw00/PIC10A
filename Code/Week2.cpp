d1cd6f59c1bf7b503f678e1a7070046856318be9
bumsu-kim <49388481+bumsu-kim@users.noreply.github.com>
2022-04-05 04:01:00 -0700

    Week 2 Tuesday

--- Code/Week2.cpp
+++ Code/Week2.cpp
@@ -3,6 +3,21 @@ using namespace std;
 
 int main() {
 
+	int n = 4.567; // what is the value of n?
+	cout << n << endl;
+
+	//// Machine Epsilon
+	//double eps = 1.0 / 1024 / 1024 / 1024 / 1024 / 1024;
+	//cout << "eps = " << eps << ", but 1+eps-1 = " << (1 + eps) - 1 << endl;
+	//eps /= 2.0;
+	//cout << "eps = " << eps << ", but 1+eps-1 = " << (1 + eps) - 1 << endl;
+	//eps /= 2.0;
+	//cout << "eps = " << eps << ", but 1+eps-1 = " << (1 + eps) - 1 << endl;
+	//eps /= 2.0;
+	//cout << "eps = " << eps << ", but 1+eps-1 = " << (1 + eps) - 1 << endl;
+	//eps /= 2.0;
+	//cout << "eps = " << eps << ", but 1+eps-1 = " << (1 + eps) - 1 << endl;
+
 	//// Type Cast
 	//double x = 5; // 5.678;
 	//int y = static_cast<int>(x);
@@ -24,6 +39,11 @@ int main() {
 	//cout << "Hello, World!" << endl; // if the output is supposed to be this,
 	//cout << "Hello World!" << endl; // this is wrong
 	//cout << "Hello, world!" << endl; // this is also wrong
+
+	// Exercise:
+	// Write a program to input a positive integer from user and calculate the product of digits
+	// Your code should work for all integers ranging from 100 to 999
+
 
 	return 0;
 }
