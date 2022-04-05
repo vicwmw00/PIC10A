41bdf53ef7f8851ad205360351d86cd86c442ab6
bumsu-kim <49388481+bumsu-kim@users.noreply.github.com>
2022-04-05 03:39:16 -0700

    Week2 Tuesday

---
+++ Code/Week2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	//// Type Cast
+	//double x = 5; // 5.678;
+	//int y = static_cast<int>(x);
+
+
+	//// Participation Question
+	//double runTime = 7345.24;
+	//int a = static_cast<int>(runTime + 0.5);
+	//int hours = a / 3600;
+	//int minutes = (a % 3600) / 60;
+	//int seconds = a % 60;
+
+	//cout << "Rounded run time: "
+	//	<< hours << " hour(s), " << minutes
+	//	<< " minute(s), " << seconds << " second(s)";
+
+	//// Output accuracy is important for your HW
+	//cout << endl;
+	//cout << "Hello, World!" << endl; // if the output is supposed to be this,
+	//cout << "Hello World!" << endl; // this is wrong
+	//cout << "Hello, world!" << endl; // this is also wrong
+
+	return 0;
+}
