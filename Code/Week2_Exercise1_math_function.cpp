75db0301e63906d38cca3e786bdabb75a9285b99
bumsu-kim <49388481+bumsu-kim@users.noreply.github.com>
2022-04-07 15:30:41 -0700

    Update Week2_Exercise1_math_function.cpp

--- Code/Week2_Exercise1_math_function.cpp
+++ Code/Week2_Exercise1_math_function.cpp
@@ -11,10 +11,24 @@
 */
 
 #include <iostream>
+#include <cmath> // enables us to use "cos" and "sin"
 
 using namespace std;
 
-int main() {
+int main() { 
+	double A = 0., B = 0.; // initialize before use
+	cout << "What is the degree of angle A?" << endl;
+	cin >> A;
+	cout << "What is the degree of angle B?" << endl;
+	cin >> B;
 
+	const double pi = 3.141592; // better coding practice (making it const double)
+	// now we convert (deg --> rad)
+	A *= (pi / 180.);
+	B *= (pi / 180.);
+	cout << "The left-hand-side of the trig identity is "
+		<< cos(A + B) << endl;
+	cout << "The right-hand-side of the trig identity is "
+		<< cos(A)*cos(B) - sin(A)*sin(B) << endl;
 	return 0;
 }
