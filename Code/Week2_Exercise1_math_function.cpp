22df27bf5cf92f0bbbcdc9d9400ebbc61a959745
bumsu-kim <49388481+bumsu-kim@users.noreply.github.com>
2022-04-06 22:29:01 -0700

    Week2 Thursday Exercises

---
+++ Code/Week2_Exercise1_math_function.cpp
@@ -0,0 +1,20 @@
+/*
+* Write a program that verifies the trigonometric identity:
+*   cos(A+B) = cos(A)cos(B) - sin(A)sin(B)
+* 
+* Input two angles A and B in *degrees* from user
+*   and convert the angles to radians
+* 
+* Then output cos(A+B) and cos(A)cos(B) - sin(A)sin(B)
+* 
+* Note: You will need to include an appropriate library
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	return 0;
+}
---
+++ Code/Week2_Exercise2_product_of_digits.cpp
@@ -0,0 +1,15 @@
+/*
+* Write a program to input a positive integer from user
+*    and calculate the product of digits
+* 
+* Your code should work for all integers ranging from 100 to 999
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	return 0;
+}
