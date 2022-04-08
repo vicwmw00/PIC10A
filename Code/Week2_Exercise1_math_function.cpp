e8a27ba8aaa2f701c4adb64b86d5a7ad4e9eee3a
bumsu-kim <49388481+bumsu-kim@users.noreply.github.com>
2022-04-07 20:45:38 -0700

    Week2 Thursday after-discussion
    Includes exercise solutions and discussion slides

--- Code/Week2_Exercise1_math_function.cpp
+++ Code/Week2_Exercise1_math_function.cpp
@@ -11,10 +11,26 @@
 */
 
 #include <iostream>
-
+#include <cmath> // to use std::cos and std::sin
 using namespace std;
 
 int main() {
+	double A = 0., B = 0.; // initializing them is a good coding practice
+	// get input from the user
+	cout << "What is the degree of angle A?\n"; // you can use '\n' instead of endl
+	cin >> A;
+	cout << "What is the degree of angle B?" << endl; // or you can use endl, of course
+	cin >> B;
 
+	// conver angles from degree to rad
+	const double pi = 3.14159265358979323846; // make it const
+	// more advanced: "constexpr double pi = 3.14159265358979323846;" is better, but we won't cover it
+	A *= pi / 180.; // from degree to pi
+	B *= pi / 180.; // from degree to pi
+	
+	// verify the trig identity
+	cout << "The left-hand-side of the trig identity evaluates to " << cos(A + B) << ".\n"; // don't forget to add "." at the end
+	cout << "The right-hand-side of the trig identity evaluates to "
+		<< cos(A)*cos(B) - sin(A)*sin(B) << ".\n"; // don't forget to add "." at the end
 	return 0;
 }
--- Code/Week2_Exercise2_product_of_digits.cpp
+++ Code/Week2_Exercise2_product_of_digits.cpp
@@ -10,6 +10,19 @@
 using namespace std;
 
 int main() {
+	// get user input
+	int three_digit_number = 0; // always better to initialize
+	cout << "Input an integer (100 - 999):\n";
+	cin >> three_digit_number;
+	
+	// calculate ones/tens/hundreds digits. (example: 132)
+	int ones = three_digit_number % 10; // 132 --> 2
+	three_digit_number /= 10; // 132 --> 13
+	int tens = three_digit_number % 10; // 13 --> 3
+	int hundreds = three_digit_number / 10; // 13 --> 1
+
+	// output the product
+	cout << "The product of digits is " << ones * tens * hundreds << endl;
 
 	return 0;
 }
